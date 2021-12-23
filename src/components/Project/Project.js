ee3b9d0c858a35bc91cb632c8e838e359ae20b9a
milosz.kucharski <miloszkucharski@gmail.com>
2021-12-23 23:26:12 +0100

    added some cool scripts so i dont have to copy emotes

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -2,11 +2,24 @@ import React from "react";
 import "./project.css";
 import Fade from 'react-reveal/Fade'
 
+function statusToEmoji(status){
+  switch(status){
+    case 'finished':
+      return '🏁';
+    case 'in_progress':
+      return '🚧';
+    case 'abandoned':
+      return '🏴‍☠️';
+    default:
+      return '';
+  }
+}
+
 function Project(props) {
   return (
     <Fade bottom>
     <div className="project">
-      <div className="status">{props.status}</div>
+      <div className="status">{statusToEmoji(props.status)}</div>
       <h4>{props.name}</h4>
       <p>
         <b>Description:</b> {props.desc}
