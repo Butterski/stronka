b60764d3ddb1196d71d52463ef2338ea3ac68764
milosz.kucharski <miloszkucharski@gmail.com>
2021-12-24 01:26:42 +0100

    added particles as bg

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import './index.css';
 import App from './App';
 import Header from './components/Header/Header'
 import TopButton from './components/TopButton/TopButton';
+import Particles from './components/Particles/Particles';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Particles />
     <Header />
     <App />
     <TopButton />
