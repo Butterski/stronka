4f432ada406ff1435ebcffada0ccfccc91a19b24
milosz.kucharski <miloszkucharski@gmail.com>
2022-01-04 17:07:51 +0100

    added things i want to learn

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import "./App.css";
 import Project from "./components/Project/Project";
-import Fade from 'react-reveal/Fade'
+import Fade from "react-reveal/Fade";
 
 function App() {
   return (
     <div className="App">
       <main>
         <div>
-        <img src="logo.svg" alt="logo" className="logo"></img>
+          <img src="logo.svg" alt="logo" className="logo"></img>
           <div className="firstview">
-          <Fade left>
-            <div className="aboutme">
-              <h1>Who am I?</h1>
-              <p>
-                I am a person with great passion to IT, especially coding and
-                administrating systems. I self-taught myself most of the things
-                I know about the things I do.
-              </p>
-            </div>
+            <Fade left>
+              <div className="aboutme">
+                <h1>Who am I?</h1>
+                <p>
+                  I am a person with great passion to IT, especially coding and
+                  administrating systems. I self-taught myself most of the
+                  things I know about the things I do.
+                </p>
+              </div>
             </Fade>
           </div>
           <div className="spenproj">
@@ -26,12 +26,8 @@ function App() {
               <h3>Languages:</h3>
               <div>
                 <img
-                  src="https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg"
-                  alt="bootstrap"
-                />
-                <img
-                  src="https://raw.githubusercontent.com/devicons/devicon/master/icons/cplusplus/cplusplus-original.svg"
-                  alt="cplusplus"
+                  src="https://cdn.worldvectorlogo.com/logos/logo-javascript.svg"
+                  alt="javascript"
                 />
                 <img
                   src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg"
@@ -42,22 +38,6 @@ function App() {
                   alt="html5"
                 />
                 <img
-                  src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg"
-                  alt="javascript"
-                />
-                <img
-                  src="https://www.vectorlogo.zone/logos/mysql/mysql-icon.svg"
-                  alt="mysql"
-                />
-                <img
-                  src="https://www.vectorlogo.zone/logos/nodejs/nodejs-icon.svg"
-                  alt="nodejs"
-                />
-                <img
-                  src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg"
-                  alt="python"
-                />
-                <img
                   src="https://reactnative.dev/img/header_logo.svg"
                   alt="reactnative"
                 />
@@ -65,11 +45,31 @@ function App() {
                   src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg"
                   alt="react"
                 />
+                <img
+                  src="https://www.vectorlogo.zone/logos/nodejs/nodejs-icon.svg"
+                  alt="nodejs"
+                />
+                <img
+                  src="https://www.vectorlogo.zone/logos/mysql/mysql-icon.svg"
+                  alt="mysql"
+                />
+                <img
+                  src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg"
+                  alt="python"
+                />
+                <img
+                  src="https://raw.githubusercontent.com/devicons/devicon/master/icons/cplusplus/cplusplus-original.svg"
+                  alt="cplusplus"
+                />
               </div>
               <h3 align="left">Other Softwares or Tools:</h3>
               <img
                 src="https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg"
                 alt="git"
+              />
+              <img
+                src="https://www.vectorlogo.zone/logos/visualstudio_code/visualstudio_code-icon.svg"
+                alt="vscode"
               />
               <img
                 src="https://raw.githubusercontent.com/devicons/devicon/master/icons/linux/linux-original.svg"
@@ -90,6 +90,45 @@ function App() {
               <img
                 src="https://download.blender.org/branding/community/blender_community_badge_white.svg"
                 alt="blender"
+              />
+              <h3 align="left">Things i want to learn in 2022:</h3>
+              <img
+                src="https://www.vectorlogo.zone/logos/vim/vim-icon.svg"
+                alt="vim"
+              />
+              <img
+                src="https://raw.githubusercontent.com/detain/svg-logos/780f25886640cef088af994181646db2f6b1a3f8/svg/redux.svg"
+                alt="redux"
+              />
+              <img
+                src="https://www.vectorlogo.zone/logos/ruby-lang/ruby-lang-icon.svg"
+                alt="ruby"
+              />
+              <img
+                src="https://www.vectorlogo.zone/logos/nginx/nginx-icon.svg"
+                alt="nginx"
+              />
+              <img
+                src="https://www.vectorlogo.zone/logos/typescriptlang/typescriptlang-icon.svg"
+                alt="typescript"
+              />
+              <a href="https://github.com/mkrl/misbrands">
+              <img
+                src="https://raw.githubusercontent.com/mkrl/misbrands/456ea8aa2c7873123dce3d746528bb9a3b6dd139/javascript.svg"
+                alt="java?"
+              />
+              </a>
+              <img
+                src="https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg"
+                alt="tailwindcss"
+              />
+              <img
+                src="https://www.vectorlogo.zone/logos/getbootstrap/getbootstrap-icon.svg"
+                alt="bootstrap"
+              />
+              <img
+                src="https://www.vectorlogo.zone/logos/microsoft_azure/microsoft_azure-icon.svg"
+                alt="azure"
               />
             </div>
             <div className="projectsCont">
