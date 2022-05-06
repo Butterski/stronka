7499af47ff9090645e539128191c08eb9e6f5dfb
milosz.kucharski <miloszkucharski@gmail.com>
2022-05-06 23:11:18 +0200

    react router

--- src/index.js
+++ src/index.js
@@ -1,20 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css';
 import App from './App';
 import Header from './components/Header/Header'
 import TopButton from './components/TopButton/TopButton';
 import Particles from './components/Particles/Particles';
-import Footer from './components/Footer/Footer'
+import Footer from './components/Footer/Footer';
+import NotFound from "./views/NotFound/NotFound";
+import Experience from "./views/Experience/Experience";
+import Skills from "./views/Skills/Skills";
+import Resume from "./views/Resume/Resume";
+import Contact from "./views/Contact/Contact";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <Particles />
     <Header />
-    <App />
+    <Routes>
+      <Route path="/" element={<App />}/>
+      <Route path="/experience" element={<Experience />}/>
+      <Route path="/skills" element={<Skills />}/>
+      <Route path="/resume" element={<Resume />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route
+      path="*"
+      element={<NotFound />}
+    />
+    </Routes>
     <Footer />
     <TopButton />
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
